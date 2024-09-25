c956f7b04c6a5949c23c822e54235d90c92a1637
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-09-25 14:09:39 +0900

    fix 02_our_app.mdx

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
-
-module.exports = withNextra()
