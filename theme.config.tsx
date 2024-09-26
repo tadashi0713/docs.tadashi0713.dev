b65097be69bd0f4b2dca7bbd9f351c2f5f0b1118
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-09-26 11:27:19 +0900

    head

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>tadashi0713 Docs</span>,
@@ -12,13 +14,24 @@ const config: DocsThemeConfig = {
       titleTemplate: "%s | tadashi0713 Docs"
     };
   },
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="tadashi0713 Docs" />
-      <meta property="og:description" content="Collections of documents created by Tadashi Nemoto(tadashi0713)." />
-    </>
-  ),
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.tadashi0713.dev' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'tadashi0713 Docs'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Collections of documents created by Tadashi Nemoto(tadashi0713)'}
+        />
+      </>
+    )
+  },
   footer: {
     text: 'tadashi0713 Docs',
   },
