9c0b4dc124afcfddc6baacd9298832c0d3e9a184
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-10-09 13:38:20 +0900

    fix description

--- theme.config.tsx
+++ theme.config.tsx
@@ -23,7 +23,7 @@ const config: DocsThemeConfig = {
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
-          property="description"
+          name="description"
           content={description}
         />
         <meta property="og:url" content={url} />
