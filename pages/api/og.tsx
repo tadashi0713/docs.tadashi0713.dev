95a56a3f6a19276f8d249927db641b56c67072d5
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-10-09 13:44:11 +0900

    fix design

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -11,26 +11,56 @@ export default function (req) {
     ? searchParams.get("title")?.slice(0, 100)
     : "My default title";
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          textAlign: "center",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {title}
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 600,
-    }
-  );
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundImage: "url(https://docs.tadashi0713.dev/og.png)",
+            backgroundColor: "#fff",
+            backgroundSize: "100% 100%",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "left",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              fontSize: 60,
+              fontStyle: "normal",
+              fontWeight: "bold",
+              color: "#000",
+              padding: "0 120px",
+              lineHeight: 1.3,
+              marginBottom: "30px",
+              wordWrap: "break-word",
+            }}
+          >
+            {title}
+          </div>
+          <div
+            style={{
+              width: "100%",
+              fontSize: 40,
+              fontStyle: "normal",
+              fontWeight: "bold",
+              color: "#000",
+              padding: "0 120px",
+              lineHeight: 1.3,
+            }}
+          >
+            📚 tadashi0713 Docs
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
 }
