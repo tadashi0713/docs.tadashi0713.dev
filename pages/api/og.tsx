01200d45fe28031f658aebb7875577cbf927c193
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-10-09 12:36:16 +0900

    init vercel/og

---
+++ pages/api/og.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default function (req) {
+  const { searchParams } = new URL(req.url);
+  const hasTitle = searchParams.has("title");
+  const title = hasTitle
+    ? searchParams.get("title")?.slice(0, 100)
+    : "My default title";
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {title}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  );
+}
