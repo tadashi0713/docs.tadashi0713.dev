e455042c7d1d0bfbf6ecad48c87361ef857de100
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-10-11 00:35:22 +0900

    add title and description for workshop

--- pages/docker-workshop-ja/_meta.ts
+++ pages/docker-workshop-ja/_meta.ts
@@ -1,12 +1,12 @@
 export default {
   "index": "Docker ワークショップの概要",
-  "02_our_app": "Part 1: アプリをコンテナ化する",
-  "03_updating_app": "Part 2: アプリを更新する",
-  "04_sharing_app": "Part 3: アプリを共有する",
-  "05_persisting_data": "Part 4: DB を保持する",
-  "06_bind_mounts": "Part 5: バインドマウントを利用する",
-  "07_multi_container": "Part 6: マルチコンテナアプリ",
-  "08_using_compose": "Part 7: Docker Compose を利用する",
-  "09_image_best": "Part 8: イメージビルドのベストプラクティス",
-  "10_what_next": "Part 9: 次のステップ"
+  "02_our_app": "パート1: アプリケーションをコンテナ化する",
+  "03_updating_app": "パート2: アプリケーションの更新",
+  "04_sharing_app": "パート3: アプリケーションを共有する",
+  "05_persisting_data": "パート5: データベースの永続化",
+  "06_bind_mounts": "パート5: バインドマウントを使用する",
+  "07_multi_container": "パート6: マルチコンテナアプリケーション",
+  "08_using_compose": "パート7: Docker Compose を使用する",
+  "09_image_best": "パート8: イメージビルドのベストプラクティス",
+  "10_what_next": "パート9: 次のステップ"
 }
