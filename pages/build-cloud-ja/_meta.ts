5489926ce5f0cb8e69760cefe5efa31801445cbf
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-10-16 19:10:53 +0900

    quoteProperties

--- pages/build-cloud-ja/_meta.ts
+++ pages/build-cloud-ja/_meta.ts
@@ -1,6 +1,6 @@
 export default {
-  setup: 'セットアップ',
-  usage: '利用方法',
-  ci: '継続的インテグレーション(CI)',
-  optimization: '最適化',
+  'setup': 'セットアップ',
+  'usage': '利用方法',
+  'ci': '継続的インテグレーション(CI)',
+  'optimization': '最適化',
 }
