1661554e20964e930eb36f20e91f0221323552c4
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-11-13 13:50:54 +0900

    view-create-sbom

--- pages/scout-ja/how-tos/_meta.ts
+++ pages/scout-ja/how-tos/_meta.ts
@@ -2,4 +2,5 @@ export default {
   'artifact-types': 'Scout を使用して異なるアーティファクトタイプを分析する',
   'configure-cli': 'Docker Scout 環境変数',
   'create-exceptions-gui': 'GUI を使用して例外を作成する',
+  'view-create-sboms': 'Docker Scout SBOMs',
 }
