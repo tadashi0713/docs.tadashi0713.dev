4dabd01ee94689cf105c4eac0152500baf168f34
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-11-13 15:25:07 +0900

    finish scout

--- pages/scout-ja/how-tos/_meta.ts
+++ pages/scout-ja/how-tos/_meta.ts
@@ -3,4 +3,5 @@ export default {
   'configure-cli': 'Docker Scout 環境変数',
   'create-exceptions-gui': 'GUI を使用して例外を作成する',
   'view-create-sboms': 'Docker Scout SBOMs',
+  'create-exceptions-vex': 'VEX を使用して例外を作成する',
 }
