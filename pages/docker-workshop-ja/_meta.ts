89260ee92a0a860861a2b434a9373475eb1d18a8
tadashi0713 <tadashi.nemoto0713@gmail.com>
2024-12-16 11:14:50 +0900

    chapter

--- pages/docker-workshop-ja/_meta.ts
+++ pages/docker-workshop-ja/_meta.ts
@@ -3,7 +3,7 @@ export default {
   '02_our_app': 'パート1: アプリケーションをコンテナ化する',
   '03_updating_app': 'パート2: アプリケーションの更新',
   '04_sharing_app': 'パート3: アプリケーションを共有する',
-  '05_persisting_data': 'パート5: データベースの永続化',
+  '05_persisting_data': 'パート4: データベースの永続化',
   '06_bind_mounts': 'パート5: バインドマウントを使用する',
   '07_multi_container': 'パート6: マルチコンテナアプリケーション',
   '08_using_compose': 'パート7: Docker Compose を使用する',
