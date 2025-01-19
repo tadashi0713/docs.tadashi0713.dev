ad355da8671966ff767225291f5689d45b511ed8
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-01-19 19:25:17 +0900

    add _meta.ts

---
+++ pages/testcontainers-guides-ja/_meta.ts
@@ -0,0 +1,20 @@
+export default {
+  'introducing-testcontainers': 'Testcontainers とは何か、なぜ使うべきなのか？',
+  'getting-started-with-testcontainers-for-java':
+    'Java で Testcontainers を使い始める',
+  'testing-spring-boot-rest-api-using-testcontainers':
+    'Java Spring Boot プロジェクトで Testcontainers を使い始める',
+  'getting-started-with-testcontainers-for-nodejs':
+    'Node.js で Testcontainers を使い始める',
+  'getting-started-with-testcontainers-for-python':
+    'Python で Testcontainers を使い始める',
+  'getting-started-with-testcontainers-for-go':
+    'Go で Testcontainers を使い始める',
+  'getting-started-with-testcontainers-for-dotnet':
+    '.NET で Testcontainers を使い始める',
+  'testing-an-aspnet-core-web-app': 'ASP.NET Core Web アプリケーションのテスト',
+  'testing-aws-service-integrations-using-localstack':
+    'LocalStack を使用した AWS サービス統合のテスト',
+  'simple-local-development-with-testcontainers-desktop':
+    'Testcontainers Desktop を使用したシンプルなローカル開発',
+}
