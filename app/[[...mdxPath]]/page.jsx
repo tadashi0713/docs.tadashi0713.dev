0f6ad1ad703a753a00f20999df29be4cc2068d0e
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-01-25 06:08:50 +0900

    rename content

---
+++ app/[[...mdxPath]]/page.jsx
@@ -0,0 +1,24 @@
+import { useMDXComponents } from 'nextra-theme-docs'
+import { generateStaticParamsFor, importPage } from 'nextra/pages'
+
+export const generateStaticParams = generateStaticParamsFor('mdxPath')
+
+export async function generateMetadata(props) {
+  const params = await props.params
+  const { metadata } = await importPage(params.mdxPath)
+  return metadata
+}
+
+export default async function Page(props) {
+  const params = await props.params
+  const result = await importPage(params.mdxPath)
+  const { default: MDXContent, useTOC, metadata, title } = result
+
+  const Wrapper = useMDXComponents().wrapper
+
+  return (
+    <Wrapper toc={useTOC()} metadata={metadata} title={title}>
+      <MDXContent {...props} params={params} />
+    </Wrapper>
+  )
+}
