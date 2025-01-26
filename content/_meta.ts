b577a1d3ec22e14a08c2c434bd939a9c68987cbe
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-01-26 15:58:23 +0900

    meta

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,8 +1,11 @@
 export default {
   'index': 'Home',
-  'docker-workshop-ja': 'Dockerワークショップ',
+  'docker-workshop-ja': 'Docker ワークショップ',
+  'docker-security-ja': 'Docker セキュリティ(WIP)',
   'scout-ja': 'Docker Scout ドキュメント',
   'build-cloud-ja': 'Docker Build Cloud ドキュメント',
+  'testcontainers-cloud-ja': 'Testcontainers Cloud ドキュメント',
+  'testcontainers-guides-ja': 'Testcontainers ガイド',
   'private': {
     'display': 'hidden',
   },
