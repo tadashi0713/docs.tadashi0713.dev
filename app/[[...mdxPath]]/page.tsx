c9d9c338532f9264a16a6458c5c1a61632eaaa8f
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-01-30 17:58:30 +0900

    add dynamic ogp

--- app/[[...mdxPath]]/page.tsx
+++ app/[[...mdxPath]]/page.tsx
@@ -4,9 +4,25 @@ import { useMDXComponents } from '../../mdx-components'
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
 export async function generateMetadata(props) {
-  const params = await props.params
-  const { metadata } = await importPage(params.mdxPath)
-  return metadata
+  const { mdxPath } = await props.params
+  const { metadata } = await importPage(mdxPath)
+  const title = metadata.title || 'tadashi0713 Docs'
+  const description =
+    metadata.description ||
+    'Collections of documents created by Tadashi Nemoto(tadashi0713)'
+  const image = `https://docs.tadashi0713.dev/api/og?title=${encodeURIComponent(title)}`
+  return {
+    title: title,
+    description: description,
+    openGraph: {
+      title: title,
+      description: description,
+      siteName: 'tadashi0713 Docs',
+      locale: 'ja_JP',
+      type: 'article',
+      image: image,
+    },
+  }
 }
 
 const Wrapper = useMDXComponents({}).wrapper
