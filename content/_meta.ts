807a06e8747cb2b96103187c3bc0cece7f3811f9
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-02-16 04:44:56 +0900

    add contact and about

--- content/_meta.ts
+++ content/_meta.ts
@@ -5,6 +5,16 @@ export default {
       'pagination': false,
     },
   },
+  about: {
+    title: 'About me',
+    type: 'page',
+    href: 'https://tadashi0713.dev',
+  },
+  contact: {
+    title: 'Contact',
+    type: 'page',
+    href: 'https://tadashi0713.dev/contact',
+  },
   'docker-workshop-ja': 'Docker ワークショップ',
   'docker-security-ja': 'Docker セキュリティ(WIP)',
   'scout-ja': 'Docker Scout ドキュメント',
