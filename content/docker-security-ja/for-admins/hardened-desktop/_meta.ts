4da565e26cc9172816b49aeb01763e019080c209
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-02-16 21:16:32 +0900

    image access management

--- content/docker-security-ja/for-admins/hardened-desktop/_meta.ts
+++ content/docker-security-ja/for-admins/hardened-desktop/_meta.ts
@@ -2,4 +2,5 @@ export default {
   'enhanced-container-isolation': 'Enhanced Container Isolation',
   'settings-management': 'Settings Management',
   'registry-access-management': 'Registry Access Management',
+  'image-access-management': 'Image Access Management',
 }
