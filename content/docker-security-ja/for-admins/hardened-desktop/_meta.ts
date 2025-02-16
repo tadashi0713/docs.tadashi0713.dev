c176d7ccab0863ea4a1ace42da67e1e9672a7905
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-02-16 20:22:49 +0900

    registry access management

--- content/docker-security-ja/for-admins/hardened-desktop/_meta.ts
+++ content/docker-security-ja/for-admins/hardened-desktop/_meta.ts
@@ -1,4 +1,5 @@
 export default {
   'enhanced-container-isolation': 'Enhanced Container Isolation',
   'settings-management': 'Settings Management',
+  'registry-access-management': 'Registry Access Management',
 }
