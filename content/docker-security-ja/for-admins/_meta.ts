9f756c5a4eecd57fa01cad8044b9d993b7bbde87
tadashi0713 <tadashi.nemoto0713@gmail.com>
2025-02-27 13:56:49 +0900

    organization access token

--- content/docker-security-ja/for-admins/_meta.ts
+++ content/docker-security-ja/for-admins/_meta.ts
@@ -2,4 +2,5 @@ export default {
   'hardened-desktop': 'Hardened Docker Desktop',
   'enforce-sign-in': 'サインインの強制',
   'single-sign-on': 'シングルサインオン',
+  'access-tokens': 'Organization access tokens',
 }
